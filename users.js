898854ff5e97e6141aae822ece0335d3d3b28508
in7570 <i7570@naver.com>
2025-10-16 19:57:30 +0900

    Feat: 유효성 검사를 위한 express-validator 적용

--- users.js
+++ users.js
@@ -3,6 +3,17 @@ const router = express.Router();
 
 router.use(express.json());
 
+import { body, validationResult } from 'express-validator';
+
+const validate = (req, res, next) => {
+  const err = validationResult(req);
+  try {
+    next();
+  } catch (err) {
+    res.status(404).json(err.array());
+  }
+};
+
 // 회원가입API
 router.post('/join', [validate], async (req, res) => {
   try {
