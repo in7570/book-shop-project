258ae54a62a714708d97a84e6085ad88cb4f97ed
in7570 <i7570@naver.com>
2025-10-17 01:24:29 +0900

    Feat: 각 API별 라우터를 import하여 사용할 수 있도록 정리

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-// express
 import express from 'express';
+import dotenv from 'dotenv'; // .env
+
 const app = express();
 
-// .env
-import dotenv from 'dotenv';
 dotenv.config();
 
 app.listen(process.env.PORT, () => {
   console.log('🚀 서버가 포트 9999에서 실행 중입니다!');
   console.log('📡 http://localhost:9999');
 });
+
+import userRouter from './routes/users.js';
+import booksRouter from './routes/books.js';
+import ordersRouter from './routes/orders.js';
+import likesRouter from './routes/likes.js';
+import cartsRouter from './routes/carts.js';
+
+app.use('/users', userRouter);
+app.use('/books', booksRouter);
+app.use('/orders', ordersRouter);
+app.use('/likes', likesRouter);
+app.use('/carts', cartsRouter);
