169b4799242cfa198ac5eef3e7dfca5120257c15
in7570 <i7570@naver.com>
2025-10-24 19:23:34 +0900

    Feat: addLike, removeLike 기능 개발

--- controller/LikeController.js
+++ controller/LikeController.js
@@ -6,6 +6,13 @@ dotenv.config();
 // 좋아요 추가
 export const addLike = async (req, res) => {
   try {
+    const { bookId } = req.params;
+    const { user_id } = req.body;
+
+    let sql = `INSERT INTO likes (user_id, liked_book_id) VALUES (?,?)`;
+    let values = [user_id, bookId];
+
+    const [results] = await connection.query(sql, values);
     res.status(StatusCodes.OK).json(results);
   } catch (err) {
     console.error(err);
@@ -16,7 +23,14 @@ export const addLike = async (req, res) => {
 // 좋아요 추가
 export const removeLike = async (req, res) => {
   try {
-    res.status(StatusCodes.OK).json('좋아요 삭제');
+    const { bookId } = req.params;
+    const { user_id } = req.body;
+
+    let sql = `DELETE FROM likes WHERE user_id = ? AND liked_book_id = ?`;
+    let values = [user_id, bookId];
+
+    const [results] = await connection.query(sql, values);
+    res.status(StatusCodes.OK).json(results);
   } catch (err) {
     console.error(err);
     res.status(StatusCodes.BAD_REQUEST).end();
