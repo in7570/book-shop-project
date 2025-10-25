71e2b78f4bd6aef39fef036ea2d62c0762a1538c
in7570 <i7570@naver.com>
2025-10-25 20:21:52 +0900

    Feat: SQL구문으로 결제(주문)하기 API에 값 INSERT하기
    1. delivery TABLE에 insert 후 17열에서 값을 3으로 고정
    2. orders TABLE에 insert 후 18열에서 값을 1로 고정
    3. orderedBook TABLE 에 이중배열로 값 한번에  insert

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -6,10 +6,38 @@ dotenv.config();
 // 결제하기
 export const order = async (req, res) => {
   try {
-    res.status(StatusCodes.OK).json();
+    const {
+      items,
+      delivery,
+      totalQuantity,
+      totalPrice,
+      userId,
+      firstBookTitle,
+    } = req.body;
+    let delivery_id = 3;
+    let order_id = 1;
+    // delivery_id 구하기
+    let sql = `INSERT INTO delivery (address, receiver, contact) VALUES (?,?,?)`;
+    let values = [delivery.address, delivery.receiver, delivery.contact];
+
+    // order_id 구하기
+    sql = `INSERT INTO orders (book_title,total_quantity,total_price,user_id,delivery_id) VALUES (?,?,?,?,?)`;
+    values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
+    // order_id = results.insertId;
+
+    sql = `INSERT INTO orderedBook (order_id,book_id,quantity) VALUES ?`;
+
+    values = [];
+    items.forEach((item) => {
+      values.push([order_id, item.book_id, item.quantity]);
+    });
+
+    const [results] = await connection.query(sql, [values]);
+
+    res.status(StatusCodes.OK).json(results);
   } catch (err) {
     console.error(err);
-    res.status(StatusCodes.BAD_REQUEST).json();
+    res.status(StatusCodes.BAD_REQUEST).end();
   }
 };
 
