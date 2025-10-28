2a86037e95a5ed33c98d781c6f6723547a3aca0c
in7570 <i7570@naver.com>
2025-10-28 20:51:41 +0900

    Feat: OrderController.js API 기능 완료
    1. 결제하기 order API, insert까지 완료 후 삭제 기능까지 추가
    2. 장바구니 내역 삭제 모듈 생성 후 1번 항목에 추가
    3. 주문 내역 조회 getOrders SELECT LEFT JOIN으로 검색
    4. 주문 상세 조회 getOrderDetail params로 orderId값 받아온 후 SELECT LEFT JOIN으로 검색

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -14,25 +14,37 @@ export const order = async (req, res) => {
       userId,
       firstBookTitle,
     } = req.body;
-    let delivery_id = 3;
-    let order_id = 1;
+
     // delivery_id 구하기
     let sql = `INSERT INTO delivery (address, receiver, contact) VALUES (?,?,?)`;
     let values = [delivery.address, delivery.receiver, delivery.contact];
+    let [results] = await connection.execute(sql, values); // [values] → values
+    let delivery_id = results.insertId;
 
     // order_id 구하기
     sql = `INSERT INTO orders (book_title,total_quantity,total_price,user_id,delivery_id) VALUES (?,?,?,?,?)`;
     values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
-    // order_id = results.insertId;
+    [results] = await connection.execute(sql, values); // [values] → values, 구조분해할당 추가
+    let order_id = results.insertId;
 
+    // items로 book_id와 quantity 조회
+    sql = `SELECT book_id, quantity FROM cartItems WHERE id IN (?) `;
+    let [order_items] = await connection.query(sql, [items]);
+    console.log('items' + items);
+    console.log(order_items);
+
+    console.log('===============');
+
+    // orderedBook에 삽입
     sql = `INSERT INTO orderedBook (order_id,book_id,quantity) VALUES ?`;
-
     values = [];
-    items.forEach((item) => {
-      values.push([order_id, item.book_id, item.quantity]);
+    order_items.forEach((row) => {
+      values.push([order_id, row.book_id, row.quantity]);
     });
+    [results] = await connection.query(sql, [values]);
 
-    const [results] = await connection.query(sql, [values]);
+    // 결제 후 기록 삭제
+    [results] = await deleteCartItems(connection, items);
 
     res.status(StatusCodes.OK).json(results);
   } catch (err) {
@@ -41,10 +53,22 @@ export const order = async (req, res) => {
   }
 };
 
+// 장바구니 내역 삭제(결제 완료 후)
+const deleteCartItems = async (connection, items) => {
+  let sql = `DELETE FROM cartItems WHERE id IN (?)`;
+  let result = await connection.query(sql, [items]);
+  return result;
+};
+
 // 주문 내역 조회
 export const getOrders = async (req, res) => {
   try {
-    res.status(StatusCodes.OK).json();
+    let sql = `SELECT orders.id, created_at,address, receiver, contact,book_title, total_quantity, total_price  
+               FROM orders LEFT JOIN delivery 
+               ON orders.delivery_id = delivery.id`;
+    let [results] = await connection.query(sql);
+
+    res.status(StatusCodes.OK).json(results);
   } catch (err) {
     console.error(err);
     res.status(StatusCodes.BAD_REQUEST).json();
@@ -54,7 +78,14 @@ export const getOrders = async (req, res) => {
 // 주문 상세 조회
 export const getOrderDetail = async (req, res) => {
   try {
-    res.status(StatusCodes.OK).json();
+    let { orderId } = req.params;
+    let sql = `SELECT books.id,title,author,price,quantity
+             FROM books LEFT JOIN orderedBook
+             ON books.id = orderedBook.book_id
+             WHERE order_id = ?`;
+    let [results] = await connection.query(sql, [orderId]);
+
+    res.status(StatusCodes.OK).json(results);
   } catch (err) {
     console.error(err);
     res.status(StatusCodes.BAD_REQUEST).json();
